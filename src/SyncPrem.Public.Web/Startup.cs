54b5e01db77f3719ca8f0b0527a7404fc74f0bf3
Daniel P. Bullington <daniel.bullington@wellengineered.us>
2018-04-07 23:32:41 -0500

    Add public web content.

---
+++ src/SyncPrem.Public.Web/Startup.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SyncPrem.Public.Web
+{
+	public class Startup
+	{
+		#region Constructors/Destructors
+
+		public Startup(IConfiguration configuration)
+		{
+			this.Configuration = configuration;
+		}
+
+		#endregion
+
+		#region Properties/Indexers/Events
+
+		public IConfiguration Configuration
+		{
+			get;
+		}
+
+		#endregion
+
+		#region Methods/Operators
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseBrowserLink();
+				app.UseDeveloperExceptionPage();
+			}
+			else
+			{
+				app.UseExceptionHandler("/error");
+			}
+
+			app.UseStaticFiles();
+			//app.UseMvc();
+		}
+
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			//services.AddMvc();
+			services.AddDirectoryBrowser();
+		}
+
+		#endregion
+	}
+}
