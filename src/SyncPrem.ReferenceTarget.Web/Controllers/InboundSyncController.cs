213c10e1bf295d1259f9d3bbcf134737e01832e8
Daniel P. Bullington <daniel.bullington@wellengineered.us>
2018-04-07 23:35:48 -0500

    Add reference web project.

---
+++ src/SyncPrem.ReferenceTarget.Web/Controllers/InboundSyncController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+
+using SyncPrem.StreamingIO.ProxyWrappers.Internal;
+
+namespace SyncPrem.ReferenceTarget.Web.Controllers
+{
+	[Route("api/[controller]")]
+	public class InboundSyncController : Controller
+	{
+		#region Methods/Operators
+
+		[HttpPost]
+		[DisableRequestSizeLimit]
+		public async Task<IActionResult> Post()
+		{
+			Task task;
+
+			task = Task.Run(() =>
+							{
+								Stream stream = this.Request.Body;
+
+								using (ProgressWrappedStream inputStream = new ProgressWrappedStream(stream))
+								{
+									using (ProgressWrappedStream outputStream = new ProgressWrappedStream(System.IO.File.Create(Path.Combine("d:\\", $"api-post-{Guid.NewGuid().ToString("N")}.json"))))
+									{
+										inputStream.CopyTo(outputStream);
+									}
+								}
+							});
+
+			await task;
+
+			if (task.IsCompletedSuccessfully)
+				return this.Ok();
+			else
+				return this.StatusCode(500);
+		}
+
+		#endregion
+	}
+}
---
+++ src/SyncPrem.ReferenceTarget.Web/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace SyncPrem.ReferenceTarget.Web
+{
+	public class Program
+	{
+		#region Methods/Operators
+
+		public static IWebHost BuildWebHost(string[] args) =>
+			WebHost.CreateDefaultBuilder(args)
+				.UseStartup<Startup>()
+				.Build();
+
+		public static void Main(string[] args)
+		{
+			BuildWebHost(args).Run();
+		}
+
+		#endregion
+	}
+}
